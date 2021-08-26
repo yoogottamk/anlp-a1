bacf5f5c0a2bed766ca1931e7eea3ef7c701312d
yoogottamk <yoogottamk@outlook.com>
2021-08-26 23:02:56 +0530

    document COM, improve interface and file-loader implementation

--- anlp_a1/com.py
+++ anlp_a1/com.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import pickle
 import random
 from collections import defaultdict
 from math import sqrt
 from pathlib import Path
 
+import numpy as np
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import svds
 from tqdm.auto import tqdm
@@ -44,12 +47,13 @@ class COMVectorizer:
         self.window_size = window_size
         self.vector_size = vector_size
 
-        self.idx2word = dict(enumerate(self.wf.keys()))
-        self.word2idx = {w: idx for (idx, w) in self.idx2word.items()}
+        self.word2idx = {w: idx for (idx, w) in enumerate(self.wf.keys())}
 
+        # the whole vocabulary won't fit in the normal way
+        # need to use a sparse matrix
+        #
+        # this dict maps from (i, j) -> value
         self.com = defaultdict(lambda: 0)
-        self.__trained = False
-
         self.features = None
 
     def subsample_probability(self, word: str, t: float = 1e-5):
@@ -72,9 +76,6 @@ class COMVectorizer:
         Generates the Co-Occurance Matrix (sparse, CSC format)
         Applies SVD
         """
-        if self.__trained:
-            return
-
         for item in tqdm(self.dataset, desc="Training COMVectorizer"):
             word_list = item["review"].split()
 
@@ -101,35 +102,78 @@ class COMVectorizer:
                     self.com[(i, j)] += 1
                     self.com[(j, i)] += 1
 
+        self.compress_com_using_svd()
+
         with open(REPO_ROOT / "com.pkl", "wb") as f:
             pickle.dump(dict(self.com), f)
+        with open(REPO_ROOT / "w2i.pkl", "wb") as f:
+            pickle.dump(dict(self.word2idx), f)
+        np.save(REPO_ROOT / "feat.npy", self.features)
 
-        self.__trained = True
+    def compress_com_using_svd(self):
+        """
+        Compute the svd for Co-Occurance Matrix to reduce feature dimension
 
-    def svd(self):
+        Converts internal COM representation into scipy CSC format and does SVD on the
+            sparse matrix
+        """
         r, c = zip(*self.com.keys())
         values = list(self.com.values())
 
         sparse_com = csc_matrix((values, (r, c)), dtype=float)
-        self.features, _, _ = svds(sparse_com, k=self.vector_size, return_singular_vectors="u")
+        self.features, _, _ = svds(
+            sparse_com, k=self.vector_size, return_singular_vectors="u"
+        )
 
     @classmethod
-    def load_from_file(cls, path: Path = REPO_ROOT / "com.pkl"):
-        with open(path, "rb") as f:
-            com = pickle.load(f)
+    def load_from_file(
+        cls,
+        feat_path: Path = REPO_ROOT / "feat.npy",
+        w2i_path: Path = REPO_ROOT / "w2i.pkl",
+    ) -> COMVectorizer:
+        """
+        Load crucial inference components from file
+
+        NOTE: the COMVectorizer obtained this way has limited functionalities
+
+        Args:
+            feat_path: path to learnt, svd-reduced Co-Occurance Matrix
+            w2i_path: path to file containing mapping for word to index
+
+        Returns:
+            COMVectorizer
+        """
+        with open(w2i_path, "rb") as f:
+            w2i = pickle.load(f)
+
+        feats = np.load(feat_path)
 
         v = cls()
-        v.com = com
-        v.__trained = True
-        v.svd()
+        v.word2idx = w2i
+        v.features = feats
 
         return v
+
+    def __getitem__(self, word: str) -> np.ndarray:
+        """
+        Gets the vector mapping for word in vocabulary
+
+        Args:
+            word: the word to get vector mapping for
+
+        Returns:
+            the vector mapping learnt by Co-Occurance Matrix method
+        """
+        return self.features[self.word2idx[word]]
 
 
 if __name__ == "__main__":
     try:
         v = COMVectorizer.load_from_file()
     except Exception as e:
-        print(e)
+        print("Couldn't load COMVectorizer from file. Training again.", e)
         v = COMVectorizer()
         v.train()
+
+    # `the` is basically guaranteed to be present in vocab
+    print(v["the"])
